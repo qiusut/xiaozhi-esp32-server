bd9e1a07dc2e8e7ad927df492a28de56d3ec5244
qiust <bz@1>
2025-05-10 11:32:44 +0800

    优化百炼TTS

--- main/xiaozhi-server/core/providers/tts/AliBL.py
+++ main/xiaozhi-server/core/providers/tts/AliBL.py
@@ -19,20 +19,26 @@ class TTSProvider(TTSProviderBase):
         dashscope.api_key = config.get("api_key")
 
     def generate_filename(self, extension=".mp3"):
-        return os.path.join(self.output_file, f"tts-{datetime.now().date()}@{uuid.uuid4().hex}{extension}")
-
+        return os.path.join(
+            self.output_file,
+            f"tts-{datetime.now().date()}@{uuid.uuid4().hex}{extension}",
+        )
     async def text_to_speak(self, text, output_file):
-        # 确保目录存在并创建空文件
-        os.makedirs(os.path.dirname(output_file), exist_ok=True)
-        with open(output_file, 'wb') as f:
-            pass
+        try:
+            synthesizer = SpeechSynthesizer(model=self.model, voice=self.voice)
+            # 确保目录存在并创建空文件
+            os.makedirs(os.path.dirname(output_file), exist_ok=True)
+            with open(output_file, "wb") as f:
+                pass
 
-        synthesizer = SpeechSynthesizer(model=self.model, voice=self.voice)
-        audio = synthesizer.call(text)
-        print('[Metric] requestId: {}, first package delay ms: {}'.format(
-            synthesizer.get_last_request_id(),
-            synthesizer.get_first_package_delay()))
+            audio = synthesizer.call(text)
+            print('[Metric] requestId: {}, first package delay ms: {}'.format(
+                synthesizer.get_last_request_id(),
+                synthesizer.get_first_package_delay()))
 
-        with open(output_file, 'ab') as f:
-            f.write(audio)
+            with open(output_file, "ab") as f:
+                f.write(audio)
 
+        except Exception as e:
+            error_msg = f"Edge TTS请求失败: {e}"
+            raise Exception(error_msg)  # 抛出异常，让调用方捕获
